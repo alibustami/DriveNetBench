7c5bf44c4d8f17c831c4f2d3a8b9dc0b2b616e41
Ali Albustami <alibustami3@gmail.com>
2024-11-05 19:45:21 +0300

    add files to transfer

--- drivenetbench/apps/cli.py
+++ drivenetbench/apps/cli.py
@@ -1,14 +1,38 @@
 """Command Line Interface for DriveNetBench."""
 
+from pathlib import Path
+from typing import Optional
+
 from typer import Typer
+
+from drivenetbench.geometry_definer import GeometryDefiner
+from drivenetbench.utilities.config import get_config
+from drivenetbench.utilities.utils import path_checker, path_fixer
 
 app = Typer()
 
 
 @app.command()
-def run():
-    """Run DriveNetBench."""
-    print("Running DriveNetBench.")
+def define_polygon(
+    output_name: str,
+    source: Path,
+    frame_num: int = 1,
+    polygon: bool = False,
+    override_if_exists: bool = False,
+):
+    """Define Polygons GUI."""
+    # if source is None:
+    #     source = get_config("digital_design_path")
+    _, fixed_source = path_checker(path_fixer(source))
+
+    geometry_definer = GeometryDefiner(
+        source=fixed_source,
+        output_name=output_name,
+        polygon=polygon,
+        frame_num=frame_num,
+        override_if_exists=override_if_exists,
+    )
+    geometry_definer.run()
 
 
 @app.command()
@@ -18,4 +42,10 @@ def test():
 
 
 if __name__ == "__main__":
-    app()
+    # run define_polygon() when the script is run
+    define_polygon(
+        "keypoints/pos3_keypoints_from_camera",
+        "assets/pos3_frame_capture.png",
+        override_if_exists=True,
+    )
+    # define_polygon("keypoints/keypoints_from_digital", "assets/track.jpg", override_if_exists=True)
