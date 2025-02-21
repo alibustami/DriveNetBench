43ec068bcfa450a8c9efd7dc85aea6a0c30a4ffd
Ali Albustami <alibustami3@gmail.com>
2025-02-21 18:00:17 -0500

    update the detector

--- drivenetbench/detector.py
+++ drivenetbench/detector.py
@@ -1,53 +1,112 @@
+from typing import Union
+
 import cv2
 import numpy as np
 import numpy.typing as npt
 from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
 import drivenetbench.utilities.config as configs
-from drivenetbench.utilities.view_transformer import ViewTransformer
 
 
 class Detector:
+    """Class for detecting the robot in the video."""
+
     def __init__(self):
+        """Initialize the Detector."""
+        self.frame_height = None
         model_path = configs.get_config(
             "benchmarker.detection_model.model_path"
         )
         self.model = YOLO(model_path)
 
-        source_keypoints = configs.get_config("view_transformer.source_path")
-        target_keypoints = configs.get_config("view_transformer.target_path")
-        self.view_transformer = ViewTransformer(
-            source=source_keypoints, target=target_keypoints
+        self.confidence_threshold = configs.get_config(
+            "benchmarker.detection_model.conf_threshold"
+        )
+        self.shift_ratio = configs.get_config(
+            "benchmarker.detection_model.shift_ratio"
         )
 
-        reference_path_file_path = configs.get_config(
-            "benchmarker.reference_track_npy_path"
-        )
-        reference_path = np.load(reference_path_file_path, dtype=np.int32)
+    def detect_robot(self, frame: npt.NDArray) -> Union[npt.NDArray, None]:
+        """Detect the robot in the frame.
 
-        track_image_path = configs.get_config("benchmarker.track_image_path")
-        self.track_image = cv2.imread(track_image_path)
+        Parameters
+        ----------
+        frame : npt.NDArray
+            The frame to detect the robot in.
 
-        for point in reference_path:
-            cv2.circle(self.track_image, tuple(point), 5, (0, 255, 0), -1)
+        Returns
+        -------
+        Union[npt.NDArray, None]
+            The centroid of the robot.
+        """
+        results, *_ = self.model(frame)
+        centriod = self._post_process(results=results)
 
-        self.cap_path = configs.get_config("benchmarker.video_path")
+        return centriod
 
-        self.max_frames = configs.get_config(
-            "actions.early_stop_after_x_frames"
-        )
+    def _post_process(self, results: Results) -> Union[npt.NDArray, None]:
+        """Post process the results.
 
-    def detect_robot(self) -> npt.ndarray:
-        cap = cv2.VideoCapture(self.cap_path)
+        Parameters
+        ----------
+        results : Results
+            The results to post process.
 
-        if not cap.isOpened():
-            raise IOError("Error opening video stream or file")
+        Returns
+        -------
+        Union[npt.NDArray, None]
+            The shifted centroid of the robot.
+        """
+        if results.obb is None:
+            return None
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
+        shifted_centroid = None
+        for box in results.obb:
+            if box.conf[0] < self.confidence_threshold:
+                continue
+
+            corners = box.xyxyxyxy.cpu().numpy().squeeze()
+            self.points = np.array(
+                [
+                    [int(corners[0][0]), int(corners[0][1])],
+                    [int(corners[1][0]), int(corners[1][1])],
+                    [int(corners[2][0]), int(corners[2][1])],
+                    [int(corners[3][0]), int(corners[3][1])],
+                ],
+                dtype=np.int32,
+            )
+
+            current_shifted_centroid = self._extract_centroid(self.points)
+            if current_shifted_centroid is not None:
+                shifted_centroid = current_shifted_centroid
                 break
+            if shifted_centroid is None:
+                return None
 
-            current_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-            if self.max_frames is not None and current_frame > self.max_frames:
-                break
+        return shifted_centroid
+
+    def _extract_centroid(self, points: npt.NDArray) -> npt.NDArray:
+        """Extract the centroid of the robot.
+
+        Parameters
+        ----------
+        points : npt.NDArray
+            The points to extract the centroid from.
+
+        Returns
+        -------
+        npt.NDArray
+            The centroid of the robot.
+        """
+        centroid = np.mean(points, axis=0)
+
+        if centroid[0] > 2700 and centroid[1] < 100:
+            return None
+
+        y_diff = self.frame_height - centroid[1]
+        shift = int(y_diff * self.shift_ratio)
+
+        shited_centroid = np.array([centroid[0], centroid[1] + shift])
+
+        return shited_centroid
