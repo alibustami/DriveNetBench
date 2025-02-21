7cfbe088a98ffcbe1e99d0b2f8009ac3052fe272
Ali Albustami <alibustami3@gmail.com>
2025-02-21 18:06:58 -0500

    update setup metadata

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ with open("requirements.txt") as f:
 setup(
     name="DriveNetBench",
     version="0.1",
+    short_description="A benchmarking tool using a single camera for robot navigation in predefined tracks.",
     packages=find_packages(include=["drivenetbench", "drivenetbench.*"]),
+    author="Ali Albustami",
+    author_email="abustami@umich.edu",
     install_requires=REQUIREMENTS,
     include_package_data=True,
     entry_points={
