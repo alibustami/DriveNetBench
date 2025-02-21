3f2bdda660063f60997984b4985adb96f0e148b7
Ali Albustami <alibustami3@gmail.com>
2025-02-21 17:54:12 -0500

    update configs

--- drivenetbench/utilities/config.py
+++ drivenetbench/utilities/config.py
@@ -1,14 +1,14 @@
 """Utility functions for the project."""
 
 import os
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from yaml import safe_load
 
 GLOBAL_CONFIGS = None
 
 
-def load_config() -> Dict:
+def load_config(config_file_path: Optional[str] = None) -> Dict:
     """Load the config.yaml file and return the configs.
 
     Returns
@@ -16,13 +16,19 @@ def load_config() -> Dict:
     Dict
         The configs from the config.yaml file.
     """
-    assert os.path.exists(
-        "config.yaml"
-    ), "Config.yaml file not found in the root directory"
-    with open("config.yaml") as f:
+    if config_file_path:
+        if not os.path.exists(config_file_path):
+            raise FileNotFoundError(
+                f"Config file {config_file_path} not found"
+            )
+    elif os.path.exists("config.yaml"):
+        config_file_path = "config.yaml"
+    else:
+        raise FileNotFoundError("Config file not found")
+    with open(config_file_path) as f:
         global GLOBAL_CONFIGS
         GLOBAL_CONFIGS = _process_configs(safe_load(f))
-        return GLOBAL_CONFIGS
+        # return GLOBAL_CONFIGS
 
 
 def _process_configs(configs: Dict) -> Dict:
@@ -66,7 +72,17 @@ def _process_configs(configs: Dict) -> Dict:
                 items[new_key] = v
         return items
 
-    return _flatten_dict(configs)
+    flattned_configs = _flatten_dict(configs)
+
+    for key, value in flattned_configs.items():
+        if not key.endswith("_path"):
+            continue
+        if not os.path.exists(value):
+            raise FileNotFoundError(
+                f"Path {value} associated with {key} not found"
+            )
+
+    return flattned_configs
 
 
 def get_config(key: str) -> Any:
@@ -84,8 +100,9 @@ def get_config(key: str) -> Any:
     """
     global GLOBAL_CONFIGS
     if GLOBAL_CONFIGS:
-        assert key in GLOBAL_CONFIGS, f"Key {key} not found in config.yaml"
+        if key not in GLOBAL_CONFIGS:
+            raise KeyError(f"Key {key} not found in config.yaml")
         return GLOBAL_CONFIGS.get(key)
     else:
-        GLOBAL_CONFIGS = load_config()
+        load_config()
         return get_config(key)
